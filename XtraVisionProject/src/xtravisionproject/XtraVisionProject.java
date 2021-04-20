b92f6a49fadfdcd52551c5c3ee8beb9023249a3f
Arthur Jardim <62599485+Arthur2810@users.noreply.github.com>
2021-04-20 15:10:46 +0100

    1

--- XtraVisionProject/src/xtravisionproject/XtraVisionProject.java
+++ XtraVisionProject/src/xtravisionproject/XtraVisionProject.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package xtravisionproject;
 
-/**
- *
- * @author artht
- */
+
 public class XtraVisionProject {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         System.out.println("Hello world test!");
         System.out.println("Hello world test!");
 
